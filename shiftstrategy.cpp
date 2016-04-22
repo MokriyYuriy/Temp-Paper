6d6f3c7e5f34a915aee24a9c63b1be2af748ce24
Mokriy Yury <mavrand@mail.ru>
2016-04-22 08:17:05 +0300

    backup

--- shiftstrategy.cpp
+++ shiftstrategy.cpp
@@ -40,7 +40,7 @@ static std::vector <int> FindAllCows(Player *player, std::size_t n, std::size_t 
 
 static std::vector <int> FindPermutation(Player *player, std::vector <int> string, std::size_t n, std::size_t m) {
     std::vector <int> answer(n);
-    if (n == 1) return {0};
+    if (n == 1) return string;
     for (std::size_t i = 0; i < n; i++, std::rotate(string.begin(), string.begin() + 1, string.end())) {
         bool is_first_answer;
         std::vector <int> first_set, second_set, bulls;
@@ -70,8 +70,67 @@ static std::vector <int> FindPermutation(Player *player, std::vector <int> strin
     return answer;
 }
 
+static int BinSearchQuery(Player *player, const std::vector <int> &string, int left,
+                          int right, std::size_t n, std::size_t m) {
+    std::vector <bool> used(min(n * 2, m), false);
+    std::vector <int> query_string(n);
+    std::size_t p = 0;
+    for (std::size_t i = 0; i < n; i++) {
+        if (string[i] < used.size()) used[string[i]] = true;
+    }
+    for (std::size_t i = 0; i < n; i++) {
+        if (i >= left && i < right) {
+            query_string[i] = string[i];
+        } else {
+            while (used[p]) p++;
+            query_string[i] = p++;
+        }
+    }
+    return (player->query(query_string)).get_bulls();
+}
+
+static std::size_t BinSearch(Player *player, const std::vector <int> &string, int left,
+                             int right, std::size_t n, std::size_t m, int cnt_bulls) {
+    while (left + 1 < right) {
+        int middle = (left + right) / 2;
+        if (BinSearchQuery(player) - cnt_bulls > 0) {
+            right = middle;
+        } else {
+            left = middle;
+        }
+    }
+    return right;
+}
+
+static std::vector <int> BinSearchFindPermutation(Player *player, std::vector <int> string, std::size_t n, std::size_t m) {
+    if (n == 1) return string;
+    std::vector <int> answer(n);
+    for (std::size_t i = 0; i < n; i++, std::rotate(string.begin(), string.begin() + 1, string.end())) {
+        Response query = player->query(string);
+        if (query.get_bulls() == n) return string;
+        int previous_bull = -1;
+        for (int j = 0; j < query.get_bulls(); j++) {
+            previous_bull = BinSearch(player, string, previous_bull, (int)n - 1, n, m, j);
+            answer[previous_bull] = string[previous_bull];
+        }
+    }
+    return answer;
+} 
+
 int SimpleShiftStrategy(Player *player, std::size_t n, std::size_t m) {
     std::vector <int> string = FindAllCows(player, n, m);
     string = FindPermutation(player, string, n, m);
+    player->query(string);
+    return player->get_score();
+}
+
+int AdvancedShiftStrategy(Player *player, std::size_t n, std::size_t m) {
+    std::vector <int> string = FindAllCows(player, n, m);
+    if (n * 2 >= m) {
+        string = BinSearchFindPermutation(player, string, n, m);
+    } else {
+        string = FindPermutation(player, string, n, m);
+    }
+    player->query(string);
     return player->get_score();
 }
